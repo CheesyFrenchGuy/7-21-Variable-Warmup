f8789447738bf467c96bd98b8c080ac1833a1f81
CheesyFrenchGuy <thatcheesyfrenchguy@gmail.com>
2020-07-21 17:01:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# firstName = input("What is your first name?: ")
+# lastName = input("What is your last name?: ")
+
+# print("Your name in reverse is: ", lastName + ", " + firstName)
+
+#LOOP increase counter variable by 1
+
+# counter = 0
+
+# for i in range(10):
+#   counter += 1
+#   print(counter)
+
+
